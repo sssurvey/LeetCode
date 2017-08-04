230ed2f5262ea1bac1dcd81ca6de23e2fa5fd3ee
sssurvey <haominshi@gmail.com>
2017-08-04 03:27:04 -0500

    update

---
+++ 476.py
@@ -0,0 +1,14 @@
+test1 = 321
+test2 = 32141
+test3 = 453
+
+def fn1 (n):
+    b ="{0:b}".format(n)
+    #int_b = int(b)
+    
+    #print (type(b))
+    #print (type(int_b))
+    final = 0
+    
+    list_b = list(map(int,b))
+    #print (list_b)
