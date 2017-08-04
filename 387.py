e963f998dcf89c951069f300ff43fe654769cf70
sssurvey <haominshi@gmail.com>
2017-08-03 22:40:33 -0500

    added

---
+++ 387.py
@@ -0,0 +1,20 @@
+#leetCode 387
+#First Unique Char in a Str
+
+def fn (s):
+    temp_list = list(s)
+    if s == 1:
+        return 0
+    for i in range(0, len(temp_list)):
+        if temp_list[i] in temp_list[i+1:len(temp_list)]:
+            pass
+        elif temp_list[i] in temp_list[0:i]:
+            pass
+        else:
+            return temp_list.index(temp_list[i])
+    return -1
+             
+
+str_t = "dasfasfdsaf" 
+
+print (fn(str_t))
