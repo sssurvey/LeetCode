685ec33e4a6b091271d6ac1f041f425d6a25c4e7
sssurvey <haominshi@gmail.com>
2017-08-05 03:28:26 -0500

    done

--- 476.py
+++ 476.py
@@ -12,3 +12,21 @@ def fn1 (n):
     
     list_b = list(map(int,b))
     #print (list_b)
+    
+    for i in range(0, len(list_b)):
+        if list_b[i] == 0:
+            list_b[i] = 1
+        else:
+            list_b[i] = 0
+        #print (list_b)
+    i = len(list_b)
+    
+    for i in range(0, len(list_b)):
+        final += list_b[len(list_b)-1-i]*(2**i)
+        print (list_b[i],"the current bin")
+        print (final,'after addition')
+    
+    
+    return final
+
+fn1(test2)
