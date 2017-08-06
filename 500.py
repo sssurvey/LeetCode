d02091a2309f3c139d6a342c0a35c5440e96fc32
sssurvey <haominshi@gmail.com>
2017-08-06 00:31:27 -0500

    update

---
+++ 500.py
@@ -0,0 +1,36 @@
+#leetCode problem 500
+#Keyboard Row
+
+r1 = list('qwertyuiopQWERTYUIOP')
+r2 = list('asdfghjklASDFGHJKL')
+r3 = list('zxcvbnmZXCVBNM')
+#print (r1[10])
+
+
+def fn1 (l):
+    list_char = []
+    flag = False
+    final = []
+    print (l)
+    for i in range (0, len(l)):
+        list_char.append(list(l[i]))
+        
+        
+    x = 0
+        
+    print(list_char)
+    for i in range(0, len(l)):
+            if list_char[i][0] in r1:
+                while x in range(0, len(list_char[i])):
+                    if list_char[i][x] in r1:
+                        x += 1
+                        print ("list_char",i,"check", "fit")
+                        flag = True
+                    else:
+                        print ("NOT working")
+                        flag = False
+                        break
+                if flag == True:
+                    final.append(l[i])
+                    flag = False
+                x = 0
