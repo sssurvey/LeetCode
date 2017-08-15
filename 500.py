c45beba9036896d2781e29c0c9ddcba22a99a9de
sssurvey <haominshi@gmail.com>
2017-08-15 14:51:02 -0500

    done

--- 500.py
+++ 500.py
@@ -34,3 +34,49 @@ def fn1 (l):
                     final.append(l[i])
                     flag = False
                 x = 0
+                
+                    
+                
+            if list_char[i][0] in r2:
+                while x in range(0, len(list_char[i])):
+                    if list_char[i][x] in r2:
+                        x += 1
+                        print ("list_char",i,"check", "fit")
+                        flag = True
+                    else:
+                        print ("NOT working")
+                        flag = False
+                        break
+                if flag == True:
+                    final.append(l[i])
+                    flag = False
+                x = 0
+                    
+                    
+                
+            if list_char[i][0] in r3:
+                 while x in range(0, len(list_char[i])):
+                    if list_char[i][x] in r3:
+                        x += 1
+                        print ("list_char",i,"check", "fit")
+                        flag = True
+                    else:
+                        print ("NOT working")
+                        flag = False
+                        break
+                 if flag == True:
+                    final.append(l[i])
+                    flag = False
+                 x = 0
+                    
+    #print (len(l))
+    return final                
+    
+                
+             
+            
+    #return final
+
+test_list = ["qwwerwq","dafsasfda","Zcxvzxcxzv","zxc"]
+
+print(fn1(test_list))
