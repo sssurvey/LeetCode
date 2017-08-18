499f3833641adcf30d42dabd866e0e308cad2d8f
sssurvey <haominshi@gmail.com>
2017-08-17 23:24:51 -0500

    done

--- 283.py
+++ 283.py
@@ -2,11 +2,12 @@
 #Move zeroes
 
 def fn1 (nums): #this function failed, leetcode says its not in place modifications
+#I understand the instruction wrong, both function passes the test, both function works, just do not return anything. leetcode prob.283 does not accept function that return objects
     for i in range(0,len(nums)):
         if nums[i] == 0:
             nums.append(0)
             nums.remove(nums[i])
-    return nums
+    return nums #this line is not needed when answering
 
 
 def fn2 (nums): #passed
@@ -17,7 +18,7 @@ def fn2 (nums): #passed
             nums[temp] = nums[i]
             temp +=1
             
-    return nums
+    return nums #this line is not needed when answering
     
     
 test_1 = [1,0,2,3,0,0,0,4]
