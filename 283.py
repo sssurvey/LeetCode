fd5da20d9d322f08012b05e930e4eb4b9f1f787b
sssurvey <haominshi@gmail.com>
2017-08-17 23:14:47 -0500

    done

--- 283.py
+++ 283.py
@@ -5,10 +5,12 @@ def fn (nums):
     for i in range(0,len(nums)):
         if nums[i] == 0:
             nums.append(0)
-            nums.pop(nums[i])
+            nums.remove(nums[i])
+    return nums
     
     
     
-    
-test_1 = [1,2,3,0,0,0,4]
-test_1 = [0,0,0,0,0,0]
+test_1 = [1,0,2,3,0,0,0,4]
+test_2 = [0,0,0,0,0,0,1]
+
+print(fn(test_1))
