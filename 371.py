225a7bad890142684391510157612e725c1b4858
sssurvey <haominshi@gmail.com>
2017-08-18 19:41:09 -0500

    added

--- 371.py
+++ 371.py
@@ -1,3 +1,21 @@
 #leetCode 371
 #Sum of two Integers
 
+def fn (a, b):
+    if b>=0:
+        for i in range(0,b):
+            a +=1
+        return a
+    if b<0:
+        for i in range(0, abs(b)):
+            a -= 1
+        return a
+    
+    
+    
+    
+    
+test_a = -12
+test_b = -8
+
+print (fn(test_a, test_b))
