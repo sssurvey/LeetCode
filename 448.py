caed7fed4edef50f7d262dc21171e868ad3c8c75
sssurvey <haominshi@gmail.com>
2017-08-20 23:07:16 -0500

    added

--- 448.py
+++ 448.py
@@ -2,8 +2,15 @@
 #find all numbers disappeared in an Array
 
 def fn (nums):
-    pass
+    list_1_sorted = sorted(nums)
+    ans_list = []
+    for i in range(1, len(list_1_sorted)):
+        if i != list_1_sorted[i]:
+            ans_list.append(i)
+    return ans_list
     
-    
+
+
     
 test_1 = [4,3,2,7,8,2,3,1]
+print(fn(test_1))
