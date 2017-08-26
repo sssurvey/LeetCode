1388df7c94678e6b1d4859acafa223673c0387bc
sssurvey <haominshi@gmail.com>
2017-08-25 21:51:09 -0500

    done

--- 507.py
+++ 507.py
@@ -2,9 +2,24 @@
 #perfect number
 
 def fn(num):
+    div = 2
+    total = 1 #one since there is always div 1
+
+    while div *div <= num:
+        if num % div == 0:
+            total += div #increase total as adding divs
+            if div *div != num:
+                total += num /div
+        div +=1
+        
+    if num > 1 and total == num:
+        return True
     
+    return False
     
+            
     
     
     
 test_1 = 28
+print (fn(test_1))
