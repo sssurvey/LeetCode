3742b286c2940426c4500482bf0973c8ae0309ca
haominshi <haominshi@gmail.com>
2018-06-22 20:37:06 -0500

    done

--- 035.py
+++ 035.py
@@ -7,9 +7,7 @@
 # thus return 2 since 5 is >3 and <4, thus should be added to pos 2
 
 def solution(nums, target):
-    if len(nums) == 1 and nums[0] >= target: return 0
-    elif len(nums) == 1 and nums[0] < target: return 1
-    elif target > nums[-1]: return len(nums)
+    if target > nums[-1]: return len(nums)
     elif target <= nums[0]: return 0
     for index in range(len(nums)):
         if target > nums[index-1] and target <= nums[index]: return index
@@ -19,4 +17,4 @@ testcase1_arr = [1, 3, 4, 8, 9]
 testcase2_arr = [6]
 testcase3_arr = [2,3]
 
-print(solution(testcase2_arr, 8))
+print(solution(testcase2_arr, 3))
