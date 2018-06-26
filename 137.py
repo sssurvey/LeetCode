69cd92f3c3c266e79b22cc3e1b08d716c784da3c
haominshi <haominshi@gmail.com>
2018-06-26 16:25:50 -0500

    create

---
+++ 137.py
@@ -0,0 +1,18 @@
+# 137
+# [2,2,2,1,0,0,0] return 1
+
+def singleNumberII(nums):
+    current = nums[0]
+    if len(nums) == 1:
+        return nums[0]
+    for index in range(1, len(nums)-1):
+        pass
+        
+
+        
+
+
+
+
+test_num = [2,2,2,1,0,0,0]
+print(singleNumberII(test_num))
