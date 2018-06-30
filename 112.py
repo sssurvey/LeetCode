aacb7299db2a054ec743691dd4c297cdbd8a96a4
haominshi <haominshi@gmail.com>
2018-06-30 18:48:04 -0500

    finish

--- 112.py
+++ 112.py
@@ -7,6 +7,19 @@
 
 # problem 112
 
+
+#      5
+#     / \
+#    4   8
+#   /   / \
+#  11  13  4
+# /  \      \
+#7    2      1
+
+# argument root = tree, sum = sum
+# return = boolean
+# True if there is a path added up to the sum, then return True
+
 class Solution:
     def hasPathSum(self, root, sum):
         """
@@ -14,6 +27,13 @@ class Solution:
         :type sum: int
         :rtype: bool
         """
+        return self.helper(root, 0, sum)
 
-        
-    
+    def helper(self, root, path, sum):
+        if root == None:                                 return False
+        if root.left == None and root.right == None:
+            if path + root.val == sum:                   return True
+            else:                                        return False
+        else:
+            return self.helper(root.left, path + root.val, sum) \
+            or self.helper(root.right, path + root.val, sum)
