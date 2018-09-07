b46f5a1000e7abb4056d874654c4f556e79d9a05
haominshi <haominshi@gmail.com>
2018-09-06 21:13:05 -0500

    Added New Solution + gitignore

---
+++ LeetCodeJava/src/test/SolutionsTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import solutions.Solution709;
+
+class SolutionsTest {
+    @Test
+    void solution709Test(){
+        assertEquals("o", Solution709.toLowerCase("O"));
+    }
+
+}
