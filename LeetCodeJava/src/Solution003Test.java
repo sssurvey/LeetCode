93374e351400b207767e68dced3648b0cb2e7bd8
haominshi <haominshi@gmail.com>
2018-10-15 14:39:39 -0500

    1 test not passed

--- LeetCodeJava/src/Solution003Test.java
+++ LeetCodeJava/src/Solution003Test.java
@@ -3,9 +3,33 @@ import static org.junit.Assert.*;
 public class Solution003Test {
 
     @org.junit.Test
-    public void solution() {
-        assertEquals("abc", Solution003.solution("abcabcbe"));
-        assertEquals("b", Solution003.solution("bbbbb"));
-        assertEquals("kew", Solution003.solution("pwwkew"));
+    public void solution1() {
+        assertEquals(3, Solution003.solution("abcabcbe"));
     }
+
+    @org.junit.Test
+    public void solution2() {
+        assertEquals(1, Solution003.solution("bbbbb"));
+    }
+
+    @org.junit.Test
+    public void solution3() {
+        assertEquals(3, Solution003.solution("pwwkew"));
+    }
+
+    @org.junit.Test
+    public void solution4() {
+        assertEquals(2, Solution003.solution("aab"));
+    }
+
+    @org.junit.Test
+    public void solution5() {
+        assertEquals(3, Solution003.solution("abcabcbb"));
+    }
+
+    @org.junit.Test
+    public void solution6() {
+        assertEquals(3, Solution003.solution("dvdf"));
+    }
+
 }
